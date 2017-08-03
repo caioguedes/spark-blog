e0764f835584c434811190e2133754cab3df4017
Caio Guedes <caio.cesar.g.souza@gmail.com>
2017-08-02 23:07:53 -0300

    Adding Post model

---
+++ src/main/java/models/Post.java
@@ -0,0 +1,23 @@
+package models;
+
+public class Post {
+
+    private String title;
+    private String body;
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return this.body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
