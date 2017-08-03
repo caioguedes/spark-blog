52f58950f9ed3474c324cbd7e5a44b867ed44245
Caio Guedes <caio.cesar.g.souza@gmail.com>
2017-08-03 00:10:43 -0300

    Adding simple logger and a template engine

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,18 +1,12 @@
-import models.Post;
-
 import static spark.Spark.*;
+
+import controllers.Index;
 
 public class Application {
 
     public static void main(String[] args) {
-        get("/", (request, response) -> {
+        staticFiles.location("/public");
 
-            Post post1 = new Post();
-            post1.setTitle("First Blog Post");
-            post1.setBody("This is the first post of blog, then... is the number 1.");
-
-            String html = "<html><head></head><body><h2>" + post1.getTitle() + "</h1><p>" + post1.getTitle() + "</p></body></html>";
-            return html;
-        });
+        get("/", (request, response) -> new Index().get(request, response));
     }
 }
---
+++ src/main/java/controllers/Index.java
@@ -0,0 +1,37 @@
+package controllers;
+
+import models.Post;
+import spark.ModelAndView;
+import spark.Response;
+import spark.Request;
+import spark.TemplateEngine;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Index {
+
+    private TemplateEngine template = new HandlebarsTemplateEngine();
+
+    public String get(Request request, Response response) {
+        Map<String, Object> data = new HashMap<>();
+
+        ArrayList<Post> posts = new ArrayList<>();
+
+        posts.add(new Post(){{
+            setTitle("#1 Welcome to the blog!");
+            setBody("<p>Well, this is the first blog post... then it's the number one.</p>");
+        }});
+
+        posts.add(new Post(){{
+            setTitle("#2 Lorem ipsum dolor sit amet, consectetur adipiscing elit!");
+            setBody("<p>Fusce faucibus quam vitae magna hendrerit sodales. Nullam scelerisque sapien quis risus porta");
+        }});
+
+        data.put("posts", posts);
+
+        return this.template.render(new ModelAndView(data, "index.hbs"));
+    }
+}
